7d403fe1f1a3ff86126deaee8d06ca16cdb6851c
ramazankayis <kayis.ramazan@gmail.com>
2022-04-17 10:03:17 +0300

    Initial commit

---
+++ src/main/java/com/ramazankayis/SpringBoot1Application.java
@@ -0,0 +1,17 @@
+package com.ramazankayis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//exclude:dahil etmemek
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+public class SpringBoot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot1Application.class, args);
+    }
+
+}
