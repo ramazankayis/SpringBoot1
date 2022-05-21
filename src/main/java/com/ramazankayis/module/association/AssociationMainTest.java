1a87a44ac4c48c46e4c775bddcb2eba9be0f597b
ramazankayis <kayis.ramazan@gmail.com>
2022-05-21 09:46:47 +0300

    Assosiciation-2

--- src/main/java/com/ramazankayis/module/association/AssociationMainTest.java
+++ src/main/java/com/ramazankayis/module/association/AssociationMainTest.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class AssociationMainTest {
+    //assosication: gevşek bağlantıdır:
     public static void main(String[] args) {
         Market market= Market.builder().marketAdi("Xyz market").marketId(12L).build();
         Musteri musteri=Musteri.builder().musteriId(44L).musteriAdi("Müşteri Adı").musteriSoyadi("MüşteriSoyadı").build();
